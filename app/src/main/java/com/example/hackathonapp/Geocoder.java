3396759cf3c875d9ec7fdc3ea855d41d4500104d
vymn53 <vymn53@student.cis.edu.hk>
2020-11-21 16:15:08 +0800

    Commited to pull from mir

---
+++ app/src/main/java/com/example/hackathonapp/Geocoder.java
@@ -0,0 +1,26 @@
+package com.example.hackathonapp;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.time.Duration;
+
+//
+//import java.net.http.HttpClient;
+//import java.net.http.HttpRequest;
+//import java.net.http.HttpResponse;
+//
+  public class Geocoder {
+//    private static final String GEOCODING_RESOURCE = "https://geocode.search.hereapi.com/v1/geocode";
+//    private static final String API_KEY = "YOUR_API_KEY";
+//
+//    final Geocoder geocoder = new Geocoder();
+//    GeocoderRequest geocoderRequest = new GeocoderRequestBuilder().setAddress(req.getParameter("location").toString()).setLanguage("en").getGeocoderRequest();
+//    GeocodeResponse geocoderResponse = geocoder.geocode(geocoderRequest);
+//    List<GeocoderResult> someList = geocoderResponse.getResults();
+//    GeocoderResult data = someList.get(0);
+//    GeocoderGeometry data_2 = data.getGeometry();
+//    BigDecimal Latitude = data_2.getLocation().getLat();
+//    BigDecimal Longitude = data_2.getLocation().getLng();
+  }
